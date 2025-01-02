266bcaf8c8abcb73301330ee61832cf5c476e603
trinityTeam-code <triarchfoundations@gmail.com>
2025-01-02 20:05:18 +0100

    Create update_database.py

---
+++ examples/update_database.py
@@ -0,0 +1,10 @@
+        wallets_collection = db["claimers"]
+        update_result = wallets_collection.update_one(
+            {"WalletAddress": recipient_wallet},  
+            {"$unset": {f"Claimable.{token_name}": ""}}  
+        )
+
+        if update_result.modified_count == 0:
+            print("Warning: No fields updated. Either the record doesn't exist or the field was already removed.")
+        else:
+            print(f"Field '{token_name}' successfully removed from 'Claimable' for wallet {recipient_wallet}.")
