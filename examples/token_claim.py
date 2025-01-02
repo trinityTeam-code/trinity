170cdf7c49ab774ac971c08d5c2ca990ecb4149e
trinityTeam-code <triarchfoundations@gmail.com>
2025-01-02 20:03:35 +0100

    Update token_claim.py

--- examples/token_claim.py
+++ examples/token_claim.py
@@ -91,18 +91,6 @@ def claim_tokens():
 
         response = solana_client.send_raw_transaction(serialized_txn)
 
-
-        wallets_collection = db["wallets"]
-
-        update_result = wallets_collection.update_one(
-            {"WalletAddress": recipient_wallet},  
-            {"$unset": {f"Claimable.{token_name}": ""}}  
-        )
-
-        if update_result.modified_count == 0:
-            print("Warning: No fields updated. Either the record doesn't exist or the field was already removed.")
-        else:
-            print(f"Field '{token_name}' successfully removed from 'Claimable' for wallet {recipient_wallet}.")
         return jsonify({"success": True }), 200
 
     except Exception as e:
